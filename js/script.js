450c6bc4c27478fa98205a0b40c2d45798189454
georges tchianga <georgestcm@gmail.com>
2017-12-09 13:10:58 -0500

    animate and nav was built

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-$(document).ready({
+$(function(){
+  var $home = $('div.home');
+  var $entryBtn = $('button#entry_btn');
 
-
-
-
-
-
-
+  $entryBtn.on('click',function() {
+    $entryBtn.hide();
+    $home.animate({
+    height: "0vh"
+  },"1000",function() { window.location = "home.html"; })
+})
+// animates the width of index when button is clicked and open home.html
 
 
 
