6dd0c6c33205d54cc7545a8a6acf31c9b6e80327
georges tchianga <georgestcm@gmail.com>
2017-12-12 09:46:41 -0500

    site updates

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $(function(){
   var $home = $('div.home');
   var $entryBtn = $('button#entry_btn');
+  var $img = $("div.img_index");
+  var $navLinks = $("a.nav_link");
 
   $entryBtn.on('click',function() {
     $entryBtn.hide();
+    $img.fadeOut();
+    $home.css("borderBottom", "5px double black");
     $home.animate({
     height: "0vh"
-  },"1000",function() { window.location = "home.html"; })
+  },"slow",function() { window.location = "home.html"; })
 })
 // animates the width of index when button is clicked and open home.html
 
-
+$navLinks.hide().fadeIn("slow");
+// hides the nav text and then animates it
 
 })
